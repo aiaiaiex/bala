39a94fbf2a885dc813a71ea156c798add4cf00a2
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-19 00:49:49 +0800

    Get window

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,3 +1,7 @@
+import bala.Window;
+
 public class App {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Window window = Window.getWindow();
+    }
 }
