278c31898c0c050cf309a84bab528b5f3d41a6c7
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-19 00:46:47 +0800

    Create private constructor

--- app/src/main/java/bala/Window.java
+++ app/src/main/java/bala/Window.java
@@ -1,5 +1,17 @@
 package bala;
 
-public class Window {
+import java.awt.GraphicsEnvironment;
+import java.awt.DisplayMode;
 
+public class Window {
+    private int width, height;
+    private String title = "Bala";
+
+    private Window() {
+        DisplayMode displayMode = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice().getDisplayMode();
+        width = displayMode.getWidth();
+        height = displayMode.getHeight();
+
+    }
 }
