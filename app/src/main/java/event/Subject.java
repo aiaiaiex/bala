b294b06fb2250764405f677a64acd2b6d11de1cf
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-21 16:15:33 +0800

    Create observers list

--- app/src/main/java/event/Subject.java
+++ app/src/main/java/event/Subject.java
@@ -1,5 +1,14 @@
 package event;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Subject {
+    private static List<Observer> observers = new ArrayList<Observer>(1);
+
     private Subject() {}
+
+    public static void addObserver(Observer observer) {
+        observers.add(observer);
+    }
 }
