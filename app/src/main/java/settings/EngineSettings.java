3b243a8c3b147f9f960db0d1898fc5e422827618
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-22 12:11:43 +0800

    Add LOG_LEVEL

--- app/src/main/java/settings/EngineSettings.java
+++ app/src/main/java/settings/EngineSettings.java
@@ -1,5 +1,9 @@
 package settings;
 
+import java.util.logging.Level;
+
 public final class EngineSettings {
+    public static final Level LOG_LEVEL = Level.FINE;
+
     private EngineSettings() {}
 }
