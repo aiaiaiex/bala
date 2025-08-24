49df550103b9dca23b31bc20b04f4714bcce4158
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-24 16:14:13 +0800

    Implement terminate

--- app/src/main/java/sound/SoundDevice.java
+++ app/src/main/java/sound/SoundDevice.java
@@ -7,6 +7,7 @@ import org.lwjgl.openal.ALC;
 import org.lwjgl.openal.ALC10;
 import org.lwjgl.openal.ALCCapabilities;
 import org.lwjgl.openal.ALCapabilities;
+import org.lwjgl.system.MemoryUtil;
 import utilities.GlobalLogger;
 
 public final class SoundDevice {
@@ -70,6 +71,35 @@ public final class SoundDevice {
 
         LOGGER.fine("Set initialized to true");
         initialized = true;
+
+        LOGGER.fine(GlobalLogger.METHOD_RETURN);
+    }
+
+    public void terminate() {
+        LOGGER.fine(GlobalLogger.METHOD_CALL);
+
+        LOGGER.fine("Check if soundDevice is even initialized");
+        if (!initialized) {
+            LOGGER.fine("soundDevice is NOT even initialized");
+            LOGGER.fine(GlobalLogger.METHOD_RETURN);
+            return;
+        }
+
+        LOGGER.fine("Get current ALC context");
+        long currentContext = ALC10.alcGetCurrentContext();
+        LOGGER.fine("Get current sound device");
+        long currentSoundDevice = ALC10.alcGetContextsDevice(currentContext);
+
+        LOGGER.fine("Make no ALC context current");
+        ALC10.alcMakeContextCurrent(MemoryUtil.NULL);
+
+        LOGGER.fine("Destroy previously current ALC context");
+        ALC10.alcDestroyContext(currentContext);
+        LOGGER.fine("Close current sound device");
+        ALC10.alcCloseDevice(currentSoundDevice);
+
+        LOGGER.fine("Set initialized to false");
+        initialized = false;
 
         LOGGER.fine(GlobalLogger.METHOD_RETURN);
     }
