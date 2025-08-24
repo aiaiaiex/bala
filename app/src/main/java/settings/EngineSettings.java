71650f406411dffb768e660bbe05d88a3e7d56d0
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-24 23:22:28 +0800

    Add ENGINE_TITLE

--- app/src/main/java/settings/EngineSettings.java
+++ app/src/main/java/settings/EngineSettings.java
@@ -5,6 +5,7 @@ import java.util.logging.Level;
 public final class EngineSettings {
     public static final Level LOG_LEVEL = Level.FINE;
 
+    public static final String ENGINE_TITLE = "Bala";
     public static final boolean DISPLAY_EDITOR = true;
 
     private EngineSettings() {}
