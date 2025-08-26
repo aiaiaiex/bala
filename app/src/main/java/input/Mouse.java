f9e571ce0f46f55486fb72aabcdbe09221c42cf7
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-26 21:30:42 +0800

    Implement mouseButtonCallback

--- app/src/main/java/input/Mouse.java
+++ app/src/main/java/input/Mouse.java
@@ -1,12 +1,14 @@
 package input;
 
 import java.util.logging.Logger;
+import org.lwjgl.glfw.GLFW;
 import logger.GlobalLogger;
 
 public final class Mouse {
     private static final Logger LOGGER = GlobalLogger.getLogger();
 
     private static double xPosition, yPosition;
+    private static boolean[] buttonPressedStates = new boolean[GLFW.GLFW_MOUSE_BUTTON_LAST + 1];
 
     private Mouse() {}
 
@@ -18,6 +20,23 @@ public final class Mouse {
 
         LOGGER.fine(() -> String.format("New xPos: %1$s", xPos));
         LOGGER.fine(() -> String.format("New yPos: %1$s", yPos));
+
+        LOGGER.fine(GlobalLogger.METHOD_RETURN);
+    }
+
+    public static void mouseButtonCallback(long glfwWindow, int button, int action, int mods) {
+        LOGGER.fine(GlobalLogger.METHOD_CALL);
+        LOGGER.fine(() -> String.format(
+                "Method called with: (glfwWindow=%1$s) (button=%2$s) (action=%3$s) (mods=%4$s)",
+                glfwWindow, button, action, mods));
+
+        if (action == GLFW.GLFW_PRESS) {
+            LOGGER.fine(() -> String.format("(button=%1$s) is pressed", button));
+            buttonPressedStates[button] = true;
+        } else if (action == GLFW.GLFW_RELEASE) {
+            LOGGER.fine(() -> String.format("(button=%1$s) is released", button));
+            buttonPressedStates[button] = false;
+        }
 
         LOGGER.fine(GlobalLogger.METHOD_RETURN);
     }
