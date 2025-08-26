574d33ad2943d7bb7e8fcea722c370867789aa55
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-26 21:18:25 +0800

    Implement cursorPositionCallback

--- app/src/main/java/input/Mouse.java
+++ app/src/main/java/input/Mouse.java
@@ -1,5 +1,24 @@
 package input;
 
+import java.util.logging.Logger;
+import logger.GlobalLogger;
+
 public final class Mouse {
+    private static final Logger LOGGER = GlobalLogger.getLogger();
+
+    private static double xPosition, yPosition;
+
     private Mouse() {}
+
+    public static void cursorPositionCallback(long glfwWindow, double xPos, double yPos) {
+        LOGGER.fine(GlobalLogger.METHOD_CALL);
+
+        xPosition = xPos;
+        yPosition = yPos;
+
+        LOGGER.fine(() -> String.format("New xPos: %1$s", xPos));
+        LOGGER.fine(() -> String.format("New yPos: %1$s", yPos));
+
+        LOGGER.fine(GlobalLogger.METHOD_RETURN);
+    }
 }
