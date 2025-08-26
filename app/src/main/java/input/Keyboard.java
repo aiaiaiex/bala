bdead99fb8dae3090f1ad22661019df055c73808
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-26 19:06:57 +0800

    Add logs

--- app/src/main/java/input/Keyboard.java
+++ app/src/main/java/input/Keyboard.java
@@ -1,17 +1,27 @@
 package input;
 
+import java.util.logging.Logger;
 import org.lwjgl.glfw.GLFW;
+import logger.GlobalLogger;
 
 public final class Keyboard {
+    private static final Logger LOGGER = GlobalLogger.getLogger();
+
     private static boolean[] keyPressedStates = new boolean[GLFW.GLFW_KEY_LAST + 1];
 
     private Keyboard() {}
 
     public static void keyCallback(long glfwWindow, int key, int scancode, int action, int mods) {
+        LOGGER.fine(GlobalLogger.METHOD_CALL);
+
         if (action == GLFW.GLFW_PRESS) {
+            LOGGER.fine(() -> String.format("(key=%1$s) is pressed", key));
             keyPressedStates[key] = true;
         } else if (action == GLFW.GLFW_RELEASE) {
+            LOGGER.fine(() -> String.format("(key=%1$s) is released", key));
             keyPressedStates[key] = false;
         }
+
+        LOGGER.fine(GlobalLogger.METHOD_RETURN);
     }
 }
