15895b6d02fd153876d98756be5ac13dde17ff54
aiaiaiex <65184264+aiaiaiex@users.noreply.github.com>
2025-08-26 18:57:57 +0800

    Implement keyCallback

--- app/src/main/java/input/Keyboard.java
+++ app/src/main/java/input/Keyboard.java
@@ -1,5 +1,17 @@
 package input;
 
+import org.lwjgl.glfw.GLFW;
+
 public final class Keyboard {
+    private static boolean[] keyPressedStates = new boolean[GLFW.GLFW_KEY_LAST + 1];
+
     private Keyboard() {}
+
+    public static void keyCallback(long glfwWindow, int key, int scancode, int action, int mods) {
+        if (action == GLFW.GLFW_PRESS) {
+            keyPressedStates[key] = true;
+        } else if (action == GLFW.GLFW_RELEASE) {
+            keyPressedStates[key] = false;
+        }
+    }
 }
